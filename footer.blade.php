45ab9231ae0e8a5442ebed98899e4ce6ff3c3e56
201900946 <102248519+201900946@users.noreply.github.com>
2022-05-17 23:33:16 +0200

    Delete footer.blade.php

--- footer.blade.php
+++
@@ -1,7 +0,0 @@
-<script src="{{url('assets/form/js/popper.min.js')}}"></script>
-<script src="{{url('assets/form/js/jquery-3.5.1.min.js')}}"></script>
-<script src="{{url('assets/form/js/boot.js')}}"></script>
-<script src="{{url('assets/form/js/backend.js')}}"></script>
-
-</body>
-</html>
