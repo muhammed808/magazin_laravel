533fbd2b4a191f8567a9fdbbac48f4a4b887c1eb
mohamed amgad <92734378+mohamedamgadd@users.noreply.github.com>
2022-05-18 01:06:00 +0200

    Add files via upload

---
+++ resources/views/admin/admin.blade.php
@@ -0,0 +1,20 @@
+
+@include('admin.header')          
+@include('admin.navbar')
+        <div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                     <h2>{{$page_titel}} </h2>   
+                    </div>
+                </div>              
+                 <!-- /. ROW  -->
+                  <hr />
+              
+                 <!-- /. ROW  -->           
+    </div>
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+@include('admin.footer')
