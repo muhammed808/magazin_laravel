7f2a8df46d06a93d634a7a38f7ed3a56db858c02
201900946 <102248519+201900946@users.noreply.github.com>
2022-05-17 23:32:42 +0200

    Add files via upload

---
+++ resources/views/auth/footer.blade.php
@@ -0,0 +1,7 @@
+<script src="{{url('assets/form/js/popper.min.js')}}"></script>
+<script src="{{url('assets/form/js/jquery-3.5.1.min.js')}}"></script>
+<script src="{{url('assets/form/js/boot.js')}}"></script>
+<script src="{{url('assets/form/js/backend.js')}}"></script>
+
+</body>
+</html>
